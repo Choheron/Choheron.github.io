e7b942b77eeb5de96f0e927df0b630c1867106c6
Thomas Campbell <trc527@gmail.com>
2021-06-12 20:11:29 -0400

    Test

--- choheron.github.io/src/App.js
+++ choheron.github.io/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Delete <code>David Crouse</code> and save to reload.
+          Poop <code>David Crouse</code> and save to reload.
         </p>
         <a
           className="App-link"
