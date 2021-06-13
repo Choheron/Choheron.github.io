8a1890f11be80449f62bfe54a8e5a91a2997ad0a
Thomas Campbell <trc527@gmail.com>
2021-06-12 21:10:39 -0400

    Test Fix

--- choheron.github.io/src/App.js
+++ choheron.github.io/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Help <code>I am Dying</code> and David to reload.
+          Help <code>I am Dying</code> and David to Help Me.
         </p>
         <a
           className="App-link"
