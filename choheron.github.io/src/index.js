31a926099478048cd63a7d6646ac96cce76afa63
Thomas Campbell <trc527@gmail.com>
2021-11-19 12:14:29 -0500

    Attempt resume link fix

--- choheron.github.io/src/index.js
+++ choheron.github.io/src/index.js
@@ -10,7 +10,7 @@ function LinkBlock() {
             <a href="https://www.linkedin.com/in/thomasrichardcampbell/" target="_blank" rel="noreferrer noopener" title="LinkedIn"><i class='fab fa-linkedin-in'></i></a>
             <a href="https://choheron.artstation.com/" target="_blank" rel="noreferrer noopener" title="ArtStation"><i class='fab fa-artstation'></i></a>
             <a href="https://github.com/Choheron" target="_blank" rel="noreferrer noopener" title="GitHub"><i class='fab fa-github'></i></a>
-            <a href="%PUBLIC_URL%/data/Thomas Campbell - Resume.pdf" target="_blank" rel="noreferrer noopener" title="Resume - Updated Nov 2021"><i class="fas fa-file"></i></a>
+            <a href="%PUBLIC_URL%/data/Thomas_Campbell_Resume.pdf" target="_blank" rel="noreferrer noopener" title="Resume - Updated Nov 2021"><i class="fas fa-file"></i></a>
             <a href="mailto: trc527@gmail.com&subject = Portfolio Contact - [NAME]" target="_blank" rel="noreferrer noopener" title="Email"><i class='fa fa-envelope'></i></a>
             </div>
         </div>
