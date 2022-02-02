2e27eb9c04a0751d7aad56ff9f782ba72c77aa23
Thomas Campbell <trc527@gmail.com>
2022-02-02 17:07:17 -0500

    Add flavor text

--- choheron.github.io/src/Homepage.js
+++ choheron.github.io/src/Homepage.js
@@ -1,16 +1,20 @@
 import { Container, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import BEVIProjectBubble from "./components/BEVIProjectBubble";
 import LinkBlock from "./components/Linkblock";
 import './css/defLayout.css';
 
 export default function Homepage() {
+    const [interests, setInterests] = useState(["Software Engineer", "Game Designer", "Programmer" , "Web Developer", "Artist"]);
+    const [currInt, setCurrInt] = useState(interests[0]);
+
     return (
         <div class="application">
             <div class="content">
                 <div class="staticHeader">
+                    <Typography variant="subtitle1" className="over">Hello, my name is</Typography>
                     <Typography variant="h1" component="h1">Thomas Campbell</Typography>
-                    <Typography variant="subtitle1" component="p">Software Engineer - Game Designer - Programmer - Web Developer - Artist</Typography>
+                    <Typography variant="subtitle1" component="p">Software Engineer - Game Designer - Programmer - Full Stack Developer - Artist</Typography>
                 </div>
                 <LinkBlock />
                 <div class="navHeader">Projects</div>
