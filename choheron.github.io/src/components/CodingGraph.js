11df0c7091b90376adb17ee48e99157411b6d297
Choheron <trc527@gmail.com>
2022-02-24 13:05:35 -0500

    Implement and Improve Experience Table

--- choheron.github.io/src/components/CodingGraph.js
+++ choheron.github.io/src/components/CodingGraph.js
@@ -7,8 +7,8 @@ import '../css/codeGraph.css';
 export default function CodingGraph() {
     return (
         <Container className="masterContainer">
-            <Typography variant="h4" className="text">Lets talk Code...</Typography>
-            <Container className="graphContainer" component={Paper}>
+            <Typography variant="h4" className="text"><i>Lets talk Code...</i></Typography>
+            <Container className="graphContainer">
                 <table>
                     <tr>
                         <th style={{width: "10%"}}></th>
@@ -17,47 +17,72 @@ export default function CodingGraph() {
                         <th style={{width: "30%"}}>Advanced</th>
                     </tr>
                     <tr>
-                        <td><Typography variant="subtitle1" className="graphText">Java</Typography></td>
+                        <td className="headerCol"><Typography variant="h5" className="graphText">Java</Typography></td>
+                        <td colSpan={3} >
+                            <Container className="experienceBar">
+                                5 Years of Regular Coursework and Experience
+                            </Container>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="headerCol"><Typography variant="h5" className="graphText">Python</Typography></td>
+                        <td colSpan={2} >
+                            <Container className="experienceBar">
+                                4 Years of Experience - Multiple Projects
+                            </Container>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="headerCol"><Typography variant="h5" className="graphText">Javascript</Typography></td>
+                        <td colSpan={2} >
+                            <Container className="experienceBar">
+                                1 Year of Rigorous Experience through Employment and Coursework
+                            </Container>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="headerCol"><Typography variant="h5" className="graphText">C</Typography></td>
+                        <td colSpan={2}>
+                            <Container className="experienceBar">
+                                2.5 Years of Experience through Coursework
+                            </Container>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="headerCol"><Typography variant="h5" className="graphText">C#</Typography></td>
+                        <td>
+                            <Container className="experienceBar">
+                                Scripting/Project Experience
+                            </Container>
+                        </td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="headerCol"><Typography variant="h5" className="graphText">C++</Typography></td>
+                        <td>
+                            <Container className="experienceBar">
+                                OpenGL, Visual Computing
+                            </Container>
+                        </td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    {/* <tr>
+                        <td className="headerCol"><Typography variant="h5" className="graphText">HTML</Typography></td>
                         <td></td>
                         <td></td>
                         <td></td>
                     </tr>
                     <tr>
-                        <td><Typography variant="subtitle1" className="graphText">Python</Typography></td>
+                        <td className="headerCol"><Typography variant="h5" className="graphText">CSS</Typography></td>
                         <td></td>
                         <td></td>
                         <td></td>
-                    </tr>
-                    <tr>
-                        <td><Typography variant="subtitle1" className="graphText">Javascript</Typography></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td><Typography variant="subtitle1" className="graphText">C</Typography></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td><Typography variant="subtitle1" className="graphText">C#</Typography></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td><Typography variant="subtitle1" className="graphText">HTML</Typography></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td><Typography variant="subtitle1" className="graphText">CSS</Typography></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                    </tr> */}
                 </table>
             </Container>
         </Container>
