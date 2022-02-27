25c11dae77177f83f15818f7d3fe450a2cc69002
Thomas Campbell <trc527@gmail.com>
2022-02-26 21:21:13 -0500

    Visual Upgrades, Package Upgrades

--- choheron.github.io/src/Homepage.js
+++ choheron.github.io/src/Homepage.js
@@ -1,24 +1,25 @@
 import { Container, Typography, Paper } from "@mui/material";
 import React, { useState } from "react";
-import BEVIProjectBubble from "./components/BEVIProjectBubble";
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
 import CodingGraph from "./components/CodingGraph";
 import LinkBlock from "./components/Linkblock";
 import './css/defLayout.css';
 
 export default function Homepage() {
-    const [interests, setInterests] = useState(["Software Engineer", "Game Designer", "Programmer" , "Web Developer", "Artist"]);
-    const [currInt, setCurrInt] = useState(interests[0]);
 
     return (
         <div class="application">
             <div class="content">
-                <div class="staticHeader">
-                    <Typography variant="subtitle1" className="over">Hello, my name is</Typography>
-                    <Typography variant="h1" component="h1">Thomas Campbell</Typography>
-                    <Typography variant="subtitle1" component="p">Software Engineer - Game Designer - Programmer - Full Stack Developer - Artist</Typography>
+                <div class="landing">
+                    <div class="staticHeader">
+                        <Typography variant="subtitle1" className="over">Hello, my name is</Typography>
+                        <Typography variant="h1" component="h1">Thomas Campbell</Typography>
+                        <Typography variant="subtitle1" component="p">Software Engineer - Game Developer - Programmer - Full Stack Developer - Artist</Typography>
+                    </div>
+                    { /* Display links to relevant social medias and websites of mine */ }
+                    <LinkBlock />
+
                 </div>
-                { /* Display links to relevant social medias and websites of mine */ }
-                <LinkBlock />
                 <CodingGraph />
             </div>
         </div>
--- choheron.github.io/src/components/CodingGraph.js
+++ choheron.github.io/src/components/CodingGraph.js
@@ -1,92 +1,91 @@
-import { Container, Typography, Paper } from "@mui/material";
-import { width } from "@mui/system";
+import { Box, Container, Typography, Paper, Slide, Grow } from "@mui/material";
 import React from "react";
 import '../css/codeGraph.css';
 
-
 export default function CodingGraph() {
+    const containerRef = React.useRef(null);
+
     return (
         <div className="body">
-            <Container className="masterContainer">
+            <Box className="masterContainer">
                 <Typography variant="h4" className="text"><i>Lets talk Code...</i></Typography>
-                <Container className="graphContainer">
-                    <table>
-                        <tr>
-                            <th style={{width: "10%"}}></th>
-                            <th style={{width: "30%"}}>Beginner</th>
-                            <th style={{width: "30%"}}>Intermediate</th>
-                            <th style={{width: "30%"}}>Advanced</th>
-                        </tr>
-                        <tr>
-                            <td className="headerCol"><Typography variant="h5" className="graphText">Java</Typography></td>
-                            <td colSpan={3} >
-                                <Container className="experienceBar">
-                                    5 Years of Regular Coursework and Experience
-                                </Container>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="headerCol"><Typography variant="h5" className="graphText">Python</Typography></td>
-                            <td colSpan={2} >
-                                <Container className="experienceBar">
-                                    4 Years of Experience - Multiple Projects
-                                </Container>
-                            </td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td className="headerCol"><Typography variant="h5" className="graphText">Javascript</Typography></td>
-                            <td colSpan={2} >
-                                <Container className="experienceBar">
-                                    1 Year of Rigorous Experience through Employment and Coursework
-                                </Container>
-                            </td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td className="headerCol"><Typography variant="h5" className="graphText">C</Typography></td>
-                            <td colSpan={2}>
-                                <Container className="experienceBar">
-                                    2.5 Years of Experience through Coursework
-                                </Container>
-                            </td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td className="headerCol"><Typography variant="h5" className="graphText">C#</Typography></td>
-                            <td>
-                                <Container className="experienceBar">
-                                    Scripting/Project Experience
-                                </Container>
-                            </td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td className="headerCol"><Typography variant="h5" className="graphText">C++</Typography></td>
-                            <td>
-                                <Container className="experienceBar">
-                                    OpenGL, Visual Computing
-                                </Container>
-                            </td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        {/* <tr>
-                            <td className="headerCol"><Typography variant="h5" className="graphText">HTML</Typography></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td className="headerCol"><Typography variant="h5" className="graphText">CSS</Typography></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr> */}
-                    </table>
-                </Container>
-            </Container>
+                <Grow 
+                    in={true}
+                    {...(true ? { timeout: 1000 } : {})}   
+                >
+                    <Container className="graphContainer" ref={containerRef}>
+                        <table>
+                            <tr>
+                                <th style={{width: "10%"}}></th>
+                                <th style={{width: "30%"}}>
+                                    <Typography variant="h6" className="graphText"><b>Beginner</b></Typography>
+                                </th>
+                                <th style={{width: "30%"}}>
+                                    <Typography variant="h6" className="graphText"><b>Intermediate</b></Typography>
+                                </th>
+                                <th style={{width: "30%"}}>
+                                    <Typography variant="h6" className="graphText"><b>Advanced</b></Typography>
+                                </th>
+                            </tr>
+                            <tr>
+                                <td className="headerCol"><Typography variant="h5" className="graphText">Java</Typography></td>
+                                <td colSpan={3} >
+                                    <Container className="experienceBar">
+                                        5 Years of Regular Coursework and Experience
+                                    </Container>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="headerCol"><Typography variant="h5" className="graphText">Python</Typography></td>
+                                <td colSpan={2} >
+                                    <Container className="experienceBar">
+                                        4 Years of Experience - Multiple Projects
+                                    </Container>
+                                </td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td className="headerCol"><Typography variant="h5" className="graphText">Javascript</Typography></td>
+                                <td colSpan={2} >
+                                    <Container className="experienceBar">
+                                        1 Year of Rigorous Experience through Employment and Coursework
+                                    </Container>
+                                </td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td className="headerCol"><Typography variant="h5" className="graphText">C</Typography></td>
+                                <td colSpan={2}>
+                                    <Container className="experienceBar">
+                                        2.5 Years of Experience through Coursework
+                                    </Container>
+                                </td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td className="headerCol"><Typography variant="h5" className="graphText">C#</Typography></td>
+                                <td>
+                                    <Container className="experienceBar">
+                                        Scripting/Project Experience
+                                    </Container>
+                                </td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td className="headerCol"><Typography variant="h5" className="graphText">C++</Typography></td>
+                                <td>
+                                    <Container className="experienceBar">
+                                        OpenGL, Visual Computing
+                                    </Container>
+                                </td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </table>
+                    </Container>
+                </Grow>
+            </Box>
         </div>
     )
 }
