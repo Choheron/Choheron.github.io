391c3193b764e6765fda20b417d178cdc80ae2e9
Thomas Campbell <trc527@gmail.com>
2024-05-28 09:55:05 -0400

    update about me

--- choheron.github.io/src/components/AboutMeBubble.js
+++ choheron.github.io/src/components/AboutMeBubble.js
@@ -7,14 +7,14 @@ export default function AboutMeBubble(props) {
 	const containerRef = React.useRef(null);
 
 	let aboutMeText = `	
-	  I'm <b>Thomas Campbell</b>, a <i>DevOps Engineer</i> based in Centreville, VA. With a professional passion for Automation,
+	  I'm <b>Thomas Campbell</b>, a <i>DevOps Engineer</i> based in Centreville, VA. With a professional passion for Creation, Automation,
 		CI/CD, Agile, and all things DevOps, I strive to constantly improve and learn! Finding satisfaction in enhancing my skills,
-		I am dedicated to creating functional and practical systems/content.
+		I am dedicated to creating functional, practical, and scaleable systems/content.
 		<br/><br/>
 		In my time as a DevOps Engineer, I have focused on Agile principles with an emphasis on pipeline creation and refactoring 
 		using Jenkins, Code Quality Gating, Kubernetes administration and upkeep, as well as extensive work with AWS including RDS, CFN, EKS, etc. 
 		<br/><br/>
-		In my free time I keep busy maintaining a homelab, going climbing, working on coding projects, woodworking, tending to my animals, and 
+		In my free time I keep busy maintaining a homelab, climbing, coding, woodworking, tending to my animals, and 
 		playing games.
     <br/><br/>
 	`
