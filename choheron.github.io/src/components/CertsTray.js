506643cbf4f4b0cc86bb42e81f8a6b2d8f42575b
Thomas Campbell <trc527@gmail.com>
2024-10-21 11:43:33 -0500

    Add Oracle Cert

--- choheron.github.io/src/components/CertsTray.js
+++ choheron.github.io/src/components/CertsTray.js
@@ -14,6 +14,9 @@ export default function CertsTray(props) {
             <a href="https://www.credly.com/badges/fd660734-8fa0-4b83-888c-fa516e14c5db/public_url" target="_blank" rel="noreferrer noopener" title="AWS Certified Solutions Architect Associate">
               <img className="certBadge" src={`${process.env.PUBLIC_URL}/images/AWS_ASA_BADGE.png`} alt="AWS Certified Solutions Architect Associate Badge" />
             </a>
+            <a href="https://catalog-education.oracle.com/ords/certview/sharebadge?id=1DC22C1B096A4FD2042A03CCF369EC9E729B222C3A1A67A6819F17F792894238" target="_blank" rel="noreferrer noopener" title="Oracle Cloud Infrastructure 2024 Foundations Associate Certification">
+              <img className="certBadge" src={`${process.env.PUBLIC_URL}/images/OCI2024FNDCFA_BADGE.png`} alt="Oracle Cloud Infrastructure 2024 Foundations Associate Certification Badge" />
+            </a>
           </div>
         </div>
 			</Box>
